af655b910314cc190c4d99417ba6274fdfeb4aaf
gousepeer3919 <111117491+gousepeer3919@users.noreply.github.com>
2022-11-25 11:28:46 +0530

    Updated checkleap.py

--- checkleap.py
+++ checkleap.py
@@ -3,7 +3,7 @@ def CheckLeap(Year):
   if ((Year % 400 == 0) or
    (Year % 100 != 0) and
    (Year % 4 == 0)):
-    return "leap year"
+    return "leap Year"
 
   else:
     return "not a leap Year"
